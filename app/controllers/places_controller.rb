2d9a9eb5280f5c9ea4d4e860e688d38f86fe4924
timrzeznik <tim.rzeznik@kellogg.northwestern.edu>
2022-02-22 03:51:49 +0000

    version 6 clean to match rubric more closely

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,20 +1,20 @@
 class PlacesController < ApplicationController
     def index
-      @places = Place.all
-    end
-
-    def new
-      @place = Place.new
-    end
-
-    def create
-      @place = Place.new(params["place"])
-      @place.save
-      redirect_to "/places"
+        @places = Place.all
     end
 
     def show
-      @place = Place.find(params["id"])
+        @place = Place.find(params["id"])
+    end
+
+    def new
+        @place = Place.new
+    end
+
+    def create
+        @place = Place.new(params["place"])
+        @place.save
+        redirect_to "/places"
     end
 
 end
